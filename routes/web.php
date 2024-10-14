f42822f1e14fc60a1dc549234a830b9036f0ae72
gmn26 <ghazynovrial@gmail.com>
2024-10-15 00:38:19 +0700

    add auth feature with laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\home\homeController;
 use App\Http\Controllers\about\tentangKamiController;
+use App\Http\Controllers\home\homeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [homeController::class, 'index'])->name('home');
 Route::get('/tentangKami', [tentangKamiController::class, 'index'])->name('about');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
